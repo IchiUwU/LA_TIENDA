13e87c2c81d1b7c422f66cef0c0da9124f72a36b
mononeitTY <mononeitor300@gmail.com>
2021-04-30 17:25:08 -0500

    arreglos al css

--- jam.js
+++ jam.js
@@ -1,2 +1,27 @@
+const btnSwitch = document.querySelector('#switch');
 
-let numero = 10
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+
+    // Guardo el modo "dark" en el Local Storage
+
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    }else{
+        localStorage.setItem('dark-mode', 'false');
+    };
+});
+
+    // Obtenemos el modo actual
+
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+}else{
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+};
+
+ 
+
